923764273211126084f7d9f226cedb7d4b798ca3
VictortheWhite <527371128@qq.com>
2015-05-25 01:20:21 +0800

    error occurs sometime when searching the Callsign. Nearly Finished though

--- CallSignLocator.js
+++ CallSignLocator.js
@@ -6,7 +6,9 @@ var path = require('path');
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
+var ServerList = new Array();
 
+//console.log(binPath);
 var childArgs = [
   path.join(__dirname, '/phantomScript.js'),
   'http://status.aprs2.net'
@@ -14,6 +16,7 @@ var childArgs = [
 
 childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
   //handle results;
+  var count = 0;
   if (err) {
     console.log(err);
   }
@@ -21,8 +24,26 @@ childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
     console.log(stderr);
   }
   if (stdout) {
-    console.log(stdout);
+    //console.log(stdout);
     //dealwith .html file of "status.apr2.net"
+    var stringList = stdout.split('\n');
+    for (var i = 0; i < stringList.length; i++) {
+      var front, rear;
+      front = stringList[i].indexOf('href=\"http');
+      rear = stringList[i].indexOf(':14501');
+      if (front == -1 || rear == -1)
+        continue;
+
+      var tempString = stringList[i].substring(front + 6, rear + 6);
+      if (tempString.length < 21)
+        continue;
+      //console.log(count++);
+      //console.log(stringList[i]);
+      console.log(tempString);
+
+      ServerList[ServerList.length] = tempString;
+    }
+
   }
 
 });
@@ -30,6 +51,10 @@ childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
 var app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.set('port', process.env.port || 3000);
 app.listen(app.get('port'), function() {
   console.log('Server started on http://localhost:3000');
@@ -50,29 +75,55 @@ app.post('/searchCallSign', function(req, res) {
   console.log(req.url);
   console.log(req.body);
 
-  res.json({
-    succ: true
+  process.on('uncaughtException', function(err) {
+    console.error('Error caught in uncaughtException event:', err);
   });
 
-  http.get('http://status.aprs2.net', function(res) {
-    console.log('shit received');
-    console.log(res.statusCode);
-    var body = "";
-    res.on('data', function(data) {
-      body += data;
-      console.log(body);
-    });
-  }).on('error', function(err) {
-    console.log(err);
-  });
-
-  //deal with body
-  //Retrive links to T2 Servers
-  //Enter T2 Servers to search
+  try {
+    searchSeverLists(0, req.body.CallSign, res);
+  } catch (e) {
+    console.error(e);
+  }
 
 });
 
-//error handler
+function searchSeverLists(ServerItr, CallSign, response) {
+    //console.log(response);
+    if (ServerItr == ServerList.length) {
+      console.log('notFound');
+      res.json({
+        succ: false
+      });
+      return;
+    }
+    console.log(ServerItr + ': ' + ServerList[ServerItr]);
+    http.get(ServerList[ServerItr++], function(res) {
+      var body = '';
+      res.on('data', function(data) {
+        console.log(data);
+        body += data;
+        //console.log(body);
+        if (body.search(RegExp('</[Hh][tT][mM][lL]>')) != -1) {
+          console.log(body);
+          console.log('end of </html>');
+          if (body.search(CallSign) == -1)
+            searchSeverLists(ServerItr, CallSign, response);
+          else {
+            console.log('shit found here')
+            response.json({
+              succ: true,
+              ServerLocation: ServerList[ServerItr]
+            });
+            return;
+          }
+        }
+      });
+    }).on('error', function(err) {
+      console.error(err);
+      searchSeverLists(++ServerItr, CallSign, response);
+    });
+  }
+  //error handler
 
 app.use(function(req, res) {
   console.log(req.url);
